a8a58e43d2cb0292fe806bd6d21115e35e0f1cf5
Adam Wolski <miniukof@gmail.com>
2016-02-14 11:46:02 +0100

    Add game loop

--- src/quake.c
+++ src/quake.c
@@ -2,9 +2,25 @@
 
 int main(int argc, char* argv[])
 {
+        /* Initialize game arguments and host */
         com_init_argv(argc, argv);
-        vid_init();
-        capture_events_test();
-        vid_close();
+        host_init();
+
+        /* Main game loop */
+        f64 oldtime = sys_get_time();
+        while(IS_RUNNING) {
+                /* Check os events */
+                sys_capture_events();
+                
+                f64 newtime = sys_get_time();
+                f64 time = newtime - oldtime;
+
+                host_frame(time);
+        }
+
+        /* End what you started */
+        host_close();
+        com_clear_argv();
+
         return 0;
 }
