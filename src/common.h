908c7b733fdda99090bbc3f4576609753c74ab9e
Adam Wolski <miniukof@gmail.com>
2016-02-14 11:49:59 +0100

    com_clear_argv function name change.

--- src/common.h
+++ src/common.h
@@ -7,5 +7,6 @@ extern Mstring* com_argv;
 extern size_t  com_argc;
 
 bool com_init_argv(const uint32 argc, char** argv);
+bool com_clear_argv();
 bool com_check_parm(const Mstring parm);
 int32 com_atoi(const Mstring str);
