829ad60b1efa8f710c8136953c6c41ad7cbcd275
Adam Wolski <miniukof@gmail.com>
2016-03-04 22:08:28 +0100

    Fix changed integer typedefs

--- src/common.h
+++ src/common.h
@@ -6,7 +6,7 @@
 extern Mstring* com_argv;
 extern size_t  com_argc;
 
-bool com_init_argv(const uint32 argc, char** argv);
+bool com_init_argv(const u32 argc, char** argv);
 bool com_clear_argv();
 bool com_check_parm(const Mstring parm);
-int32 com_atoi(const Mstring str);
+i32 com_atoi(const Mstring str);
