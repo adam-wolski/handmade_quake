27cd997ddbd10e1d783be804a15cd5b505812f6f
Adam Wolski <miniukof@gmail.com>
2016-03-19 16:10:47 +0100

    Pack load test

--- tests/pack_tests.c
+++ tests/pack_tests.c
@@ -7,6 +7,11 @@ MU_TEST(test_pack_load) {
         MString path = mstr_from_cstr(pak_path);
         Pack* pack = pack_load(path);
         mu_assert(pack, "Failed to create pack");
+        mu_assert(pack->num_files == 339, "Found wrong number of files.");
+        mu_assert(strcmp(pack->packname, (const char*)path->data) == 0,
+                  "Wrong packname.");
+        mu_assert(strcmp(pack->pack_files[0].filename, "sound/items/r_item1.wav") == 0,
+                  "Wrong first file.");
 
         free(pack);
         mstr_destroy(path);
