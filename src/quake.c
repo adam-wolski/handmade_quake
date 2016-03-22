7f3e80d02cf2fca0b752b6db5d7e320189edc7cb
Adam Wolski <miniukof@gmail.com>
2016-03-22 18:01:25 +0100

    Just for testing draw menuplyr.lmp

--- src/quake.c
+++ src/quake.c
@@ -5,6 +5,14 @@ int main(int argc, char* argv[])
         /* Initialize game arguments and host */
         com_arg_init(argc, argv);
         host_init();
+        
+
+        /* XXX Just for testing, drawing menuplyr.lmp */
+        MString plyr = mstr_from_cstr("gfx/menuplyr.lmp");
+        u8* chrlmp = com_file_find(plyr, NULL);
+        draw_lump(0, 0, chrlmp);
+        mstr_destroy(plyr);
+        free(chrlmp);
 
         /* Main game loop */
         f64 oldtime = sys_get_time();
